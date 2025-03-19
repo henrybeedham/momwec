503eebc7dd29135ba101d77e595011aeb96219d7
Henry Beedham <harry@beedham.org>
2025-03-19 11:34:15 +0000

    rooms

--- src/app/[gameId]/page.tsx
+++ src/app/[gameId]/page.tsx
@@ -109,6 +109,9 @@ export default function Home() {
       console.log('Connected to Socket.IO server with id:', socket.id);
     });
 
+    // Join the game room
+    socket.emit('joinGame', gameId);
+
     // Listen for 'gameMove' events from the server
     socket.on('gameMove', (data) => {
       console.log('Received gameMove event:', data);
@@ -119,7 +122,7 @@ export default function Home() {
     return () => {
       if (socket) socket.disconnect();
     };
-  }, []);
+  }, [gameId]);
 
   const sendGameMove = () => {
     if (socket) {
