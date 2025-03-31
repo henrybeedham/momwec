ed5114a7544669b8eb2fa368f7ae38e477f088e1
Henry Beedham <harry@beedham.org>
2025-03-31 16:57:27 +0100

    lovely

--- src/components/monopoly/Popups.tsx
+++ src/components/monopoly/Popups.tsx
@@ -1,4 +1,4 @@
-import { PropertySquare } from "~/models/Square";
+import { BuyableSquare, PropertySquare } from "~/models/Square";
 import { Button } from "../ui/button";
 import {
   Dialog,
@@ -12,6 +12,7 @@ import { useState } from "react";
 import SquareCard from "./SquareCard";
 import { propertyColors } from "~/utils/monopoly";
 import { GameState } from "~/models/GameState";
+import { useUser } from "@clerk/nextjs";
 
 type PopupProps = {
   game: GameState;
@@ -22,18 +23,27 @@ type PopupProps = {
 function Popups({ game, buyProperty, passProperty }: PopupProps) {
   const property = game
     .getBoard()
-    .getSquareFromIndex(game.getSelectedProperty() ?? -1) as PropertySquare;
+    .getSquareFromIndex(game.getSelectedProperty() ?? -1) as BuyableSquare;
+
+  const { user } = useUser();
+  if (!user) throw new Error("No user");
+  const myTurn = user.id === game.getCurrentPlayer().id;
+  const shouldBeOpen = !!property && myTurn;
   return (
-    <Dialog open={!!property} >
-      {property && (
+    <Dialog open={shouldBeOpen}>
+      {!!property && (
         <DialogContent className="sm:max-w-[425px]">
           <DialogHeader>
-            <DialogTitle>You landed on {property?.name}</DialogTitle>
+            <DialogTitle>You landed on {property.name}</DialogTitle>
           </DialogHeader>
 
           <SquareCard
             square={property}
-            colourClass={propertyColors[property.group] ?? "bg-gray-200"}
+            colourClass={
+              property instanceof PropertySquare
+                ? (propertyColors[property.group] ?? "bg-gray-200")
+                : "bg-gray-200"
+            }
           />
 
           <DialogFooter>
